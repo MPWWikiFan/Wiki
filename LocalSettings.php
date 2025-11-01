86f5e45b15514db5f30e8496292a962cdc6bf3ce
MPWWikiFan <mawong31@icloud.com>
2025-11-01 16:44:34 -0400

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -1,1 +1,12 @@
 <?php
+
+//If MediaWiki is not defined, terminate the code to prevent unauthorized access. Then, invoke the config registry to retreive specific settings. Finally, define the name of the website.
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo "This file is part of MediaWiki and is not a valid entry point\n";
+	die( 1 );
+}
+$wgConfigRegistry = [
+	'main' => 'GlobalVarConfig::newInstance',
+];
+$wgSitename = 'MyWiki';
