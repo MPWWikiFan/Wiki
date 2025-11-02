dffc219c71aca64b31a9aced538d84aeea12a178
MPWWikiFan <mawong31@icloud.com>
2025-11-02 16:27:00 -0500

    Define basic settings and some file upload config

--- LocalSettings.php
+++ LocalSettings.php
@@ -1,6 +1,8 @@
 <?php
 
-//If MediaWiki is not defined, terminate the code to prevent unauthorized access. Then, invoke the config registry to retreive specific settings. Finally, define the name of the website.
+// If MediaWiki is not defined, terminate the code to prevent unauthorized access.
+// Then, invoke the config registry to retreive specific settings. 
+// Finally, define the name of the server, basic database information, and the name of the website
 
 if ( !defined( 'MEDIAWIKI' ) ) {
 	echo "This file is part of MediaWiki and is not a valid entry point\n";
@@ -9,4 +11,29 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgConfigRegistry = [
 	'main' => 'GlobalVarConfig::newInstance',
 ];
-$wgSitename = 'MyWiki';
+$wgServer = false; // Since there is no server attached to this code, this value is 'false' by default and the following DB settings are the just the defaults
+$wgCanonicalServer = false;
+$wgDBname = 'my_wiki';
+$wgDBserver = 'localhost';
+$wgDBtype = 'mysql';
+$wgSitename = 'Matthews_Wiki'; // The user-facing website name as it will appear in search results etc.
+
+// File uploads configuration
+
+$wgEnableUploads = true; 
+$wgAllowCopyUploads = true; 
+$wgCopyUploadsFromSpecialUpload = true;
+$wgFileExtensions = [ 'png', 'gif', 'jpg', 'jpeg', 'webp', ]; 
+$wgCheckFileExtensions = true; 
+$wgStrictFileExtensions = true; 
+$wgProhibitedFileExtensions = [
+	'html', 'htm', 'js', 'jsb', 'mhtml', 'mht', 'xhtml', 'xht',
+	'php', 'phtml', 'php3', 'php4', 'php5', 'phps', 'phar', 'shtml', 'jhtml', 'pl', 'py', 'cgi',
+	'exe', 'scr', 'dll', 'msi', 'vbs', 'bat', 'com', 'pif', 'cmd', 'vxd', 'cpl', 'xml',
+];
+$wgMaxUploadSize = 1000000000;
+$wgUploadMaintenance = false;
+$wgUseImageResize = true;
+$wgGenerateThumbnailOnParse = true;
+$wgShowArchiveThumbnails = true;
+$wgThumbLimits = [120, 150, 180, 200, 250, 300];
