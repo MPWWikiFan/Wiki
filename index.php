a683bb893cd1a0e06e2bcb28cda28ee8a4cd091f
MPWWikiFan <mawong31@icloud.com>
2025-11-20 12:41:57 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,15 +7,11 @@ use MediaWiki\MediaWikiServices;
 
 define( 'MW_ENTRY_POINT', 'index' );
 
-// Bail on old versions of PHP, or if composer has not been run yet to install
-// dependencies. Using dirname( __FILE__ ) here because __DIR__ is PHP5.3+.
-// phpcs:ignore MediaWiki.Usage.DirUsage.FunctionFound
 require_once dirname( __FILE__ ) . '/includes/PHPVersionCheck.php';
 wfEntryPointCheck( 'html', dirname( $_SERVER['SCRIPT_NAME'] ) );
 
 require __DIR__ . '/includes/WebStart.php';
 
-// Create the entry point object and call run() to handle the request.
 ( new ActionEntryPoint(
 	RequestContext::getMain(),
 	new EntryPointEnvironment(),
